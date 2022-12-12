b8345f1fea9eaf3addea962a7eac1cb2ec4c919d
arhandev <arhandev.code@gmail.com>
2022-12-13 05:33:34 +0700

    source code

---
+++ webinar/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+	return (
+		<ArticleContainer>
+			<p>Ini adalah Body dari Article</p>
+			<button>Klik untuk melakukan highlight</button>
+		</ArticleContainer>
+	);
+}
+
+export default Test;
